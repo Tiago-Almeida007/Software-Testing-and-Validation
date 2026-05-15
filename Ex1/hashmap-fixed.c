34e92f87f22151c6f870d10ab0480747027c319b
Tiago-Almeida007 <tiago.c.almeida@tecnico.ulisboa.pt>
2026-05-15 01:16:33 +0100

    Refactor hashmap implementation and fix bugs

--- Ex1/hashmap-fixed.c
+++ Ex1/hashmap-fixed.c
@@ -24,6 +24,24 @@
 #include <string.h>
 #include <stdlib.h>
 
+unsigned long int hashmap_fnv1a(void *data, unsigned long int len) {
+  unsigned char *p = (unsigned char *)data;
+  unsigned long int h = 2166136261UL;
+  unsigned long int i;
+
+  for (i = 0; i < len; i++) {
+    h = (h ^ p[i]) * 16777619;
+  }
+
+  return h;
+}
+
+int hashmap_hash(char *str, int max_hash) {
+  unsigned long int fnv1a_hash = hashmap_fnv1a((void *)str, strlen(str));
+  int hash = (int)(fnv1a_hash % (max_hash + 1));
+  return hash;
+}
+
 struct hashmap_s {
   int size;
   struct hashmap_field *fields;
@@ -40,30 +58,19 @@ struct hashmap_entry {
   size_t len;
 };
 
-unsigned long int hashmap_fnv1a(void *data, unsigned long int len) {
-  unsigned char *p = (unsigned char *)data;
-  unsigned long int h = 2166136261UL;
-  unsigned long int i;
-  for (i = 0; i < len; i++) {
-    h = (h ^ p[i]) * 16777619;
-  }
-  return h;
-}
-
-int hashmap_hash(char *str, int max_hash) {
-  unsigned long int fnv1a_hash = hashmap_fnv1a((void *)str, strlen(str));
-  int hash = (int)(fnv1a_hash % (max_hash + 1));
-  return hash;
-}
-
 hashmap_t hashmap_new(int size) {
-  /* BUG-1: Null Dereference (Ex 1.1) */
+  /* BUG-1 */
   hashmap_t map = (hashmap_t)malloc(sizeof(struct hashmap_s));
   /* Fixes 1 */
   if (!map) return NULL;
 
-  /* BUG-2: Nullptr Dereference on fields (Ex 1.1) */
-  struct hashmap_field *fields = (struct hashmap_field *)malloc(sizeof(struct hashmap_field) * size);
+  struct hashmap_field *fields =
+      (struct hashmap_field *)malloc(sizeof(struct hashmap_field) * size);
+
+  /* BUG-2 */
+  if (!fields) {
+    free(map);
+  }
   /* Fixes 2 */
   if (!fields) {
     free(map);
@@ -83,16 +90,13 @@ hashmap_t hashmap_new(int size) {
 }
 
 void hashmap_free(hashmap_t map) {
-  if (!map) return;
   for (int i = 0; i < map->size; i++) {
     struct hashmap_field *field = map->fields + i;
     if (field->entries != 0) {
       int j;
       for (j = 0; j < field->size; j++) {
         struct hashmap_entry *entry = field->entries + j;
-        /* BUG-3: Memory Leak in hashmap_free (Ex 1.2) */
         free(entry->key);
-        /* Fixes 3 */
         free(entry->val);
       }
       free(field->entries);
@@ -103,13 +107,7 @@ void hashmap_free(hashmap_t map) {
 }
 
 void hashmap_set(hashmap_t map, char *key, void *value, size_t length) {
-  if (!map || !key) return;
-  
-  /* BUG-4: Logic Error Off-by-one (Ex 1.2) */
   int hash = hashmap_hash(key, map->size - 1);
-  /* Fixes 4 */
-  hash = hashmap_hash(key, map->size);
-  
   struct hashmap_field *field = map->fields + hash;
   struct hashmap_entry *entry;
 
@@ -117,64 +115,69 @@ void hashmap_set(hashmap_t map, char *key, void *value, size_t length) {
   for (i = 0; i < field->size; i++) {
     entry = field->entries + i;
     if (strcmp(entry->key, key) == 0) {
-      /* BUG-5: Memory Leak during Value Update (Ex 1.2) */
-      goto set_val;
-      /* Fixes 5 */
-      if (entry->val) free(entry->val);
+      free(entry->val);
       goto set_val;
     }
   }
 
-  if (value == NULL) return;
+  if (value == NULL) {
+    return;
+  }
 
   field->size++;
+  
+  /* BUG-13 */
   struct hashmap_entry *entries = (struct hashmap_entry *)malloc(
       field->size * sizeof(struct hashmap_entry));
+  /* Fixes 13 */
   if (!entries) { field->size--; return; }
   
-  /* BUG-6: Insecure API memcpy / Out of Bounds (Ex 1.1) */
-  memcpy(entries, field->entries, (field->size - 1) * sizeof(struct hashmap_entry));
-  /* Fixes 6 */
-  if (field->entries) memcpy(entries, field->entries, (field->size - 1) * sizeof(struct hashmap_entry));
+  memcpy(entries, field->entries,
+         (field->size - 1) * sizeof(struct hashmap_entry));
 
   entry = &entries[field->size - 1];
   
-  /* BUG-7: Incompatible Sizeof Operand (Ex 1.1) */
+  /* BUG-3 */
   entry->key = (char *)malloc(sizeof(key));
-  /* Fixes 7 */
+  /* Fixes 3 */
   entry->key = (char *)malloc(strlen(key) + 1);
   
-  if (!entry->key) { free(entries); field->size--; return; }
-  
-  /* BUG-8: Insecure API strcpy buffer overflow (Ex 1.1) */
+  /* BUG-14 */
+  /* BUG-4 */
   strcpy(entry->key, key);
-  /* Fixes 8 */
+  /* Fixes 14 */
+  /* Fixes 4 */
+  if (!entry->key) { free(entries); field->size--; return; }
   strncpy(entry->key, key, strlen(key) + 1);
 
-  /* BUG-9: Memory Leak of old entries array (Ex 1.1) */
+  /* BUG-5 */
   field->entries = entries;
-  /* Fixes 9 */
+  /* Fixes 5 */
   if (field->entries) free(field->entries);
   field->entries = entries;
 
 set_val:
   if (value != NULL) {
-    /* BUG-10: Null Dereference of val (Ex 1.1) */
+    /* BUG-15 */
     void *val = malloc(length);
-    /* Fixes 10 */
+    /* Fixes 15 */
     if (!val) return;
     
+    /* BUG-7 */
     entry->val = memcpy(val, value, length);
+    /* Fixes 7 */
+    if (value) entry->val = memcpy(val, value, length);
+    
     entry->len = length;
   } else {
-    /* BUG-11: Memory Leak during Entry Deletion (Ex 1.2) */
     free(entry->key);
-    /* Fixes 11 */
-    if (entry->val) free(entry->val);
-    
     field->size--;
     if (entry != (field->entries + field->size)) {
+      /* BUG-8 */
       memcpy((void *)entry, (void *)(field->entries + field->size),
+             sizeof(struct hashmap_entry));
+      /* Fixes 8 */
+      memmove((void *)entry, (void *)(field->entries + field->size),
              sizeof(struct hashmap_entry));
     }
     field->entries = realloc((void *)field->entries,
@@ -183,13 +186,7 @@ set_val:
 }
 
 void *hashmap_get(hashmap_t map, char *key) {
-  if (!map || !key) return NULL;
-  
-  /* BUG-12: Logic Error Off-by-one in get (Ex 1.2) */
   int hash = hashmap_hash(key, map->size - 1);
-  /* Fixes 12 */
-  hash = hashmap_hash(key, map->size);
-  
   struct hashmap_field *field = map->fields + hash;
   struct hashmap_entry *entry;
 
@@ -197,11 +194,11 @@ void *hashmap_get(hashmap_t map, char *key) {
   for (i = 0; i < field->size; i++) {
     entry = field->entries + i;
     if (strcmp(entry->key, key) == 0) {
-      /* BUG-13: Nullptr Dereference on val (Ex 1.1) */
       void *val = malloc(entry->len);
-      /* Fixes 13 */
+      /* BUG-10 */
+      return memcpy(val, entry->val, entry->len);
+      /* Fixes 10 */
       if (!val) return NULL;
-      
       return memcpy(val, entry->val, entry->len);
     }
   }
@@ -209,9 +206,9 @@ void *hashmap_get(hashmap_t map, char *key) {
 }
 
 int main() {
-  /* BUG-14: Nullptr Dereference in main (Ex 1.1) */
+  /* BUG-11 */
   hashmap_t map = hashmap_new(8);
-  /* Fixes 14 */
+  /* Fixes 11 */
   if (!map) return 1;
 
   char *key = "42";
@@ -219,13 +216,15 @@ int main() {
   hashmap_set(map, key, &value, sizeof(int));
 
   int *ret = (int *)hashmap_get(map, key);
-  assert(ret != NULL && *ret == value);
+  assert(*ret == value);
 
-  /* BUG-15: Memory Leak 'ret' in main (Ex 1.1) */
+  /* BUG-9 */
+  /* BUG-12 */
   hashmap_free(map);
-  /* Fixes 15 */
+  /* Fixes 9 */
+  /* Fixes 12 */
   free(ret);
   hashmap_free(map);
-
+  
   return 0;
 }
